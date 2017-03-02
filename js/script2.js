f4446a9c5a8292ace7c4edfbd45b7f81ce0d77f0
vglyadkovskiy <glycerine08@gmail.com>
2017-03-02 16:03:50 +0200

    HW3_2
    Сделал тест строгим

--- js/script2.js
+++ js/script2.js
@@ -1,11 +1,15 @@
 var rad11 = document.getElementById("rad11");
 var rad23 = document.getElementById("rad23");
-var rad33 = document.getElementById("rad33");
+var rad31 = document.getElementById("rad31");
 var bres = document.getElementById("buttonResult");
+var chb11 = document.getElementById("chb11");
 var chb12 = document.getElementById("chb12");
 var chb13 = document.getElementById("chb13");
+var chb14 = document.getElementById("chb14");
 var chb21 = document.getElementById("chb21");
 var chb22 = document.getElementById("chb22");
+var chb23 = document.getElementById("chb23");
+var chb24 = document.getElementById("chb24");
 
 function chekRadio(){
    var radRes = 0;
@@ -15,7 +19,7 @@ function chekRadio(){
    if (rad23.checked == true){
       radRes += 1;
    }
-   if (rad33.checked == true){
+   if (rad31.checked == true){
       radRes += 1;
    }
    return radRes;
@@ -23,17 +27,23 @@ function chekRadio(){
 
 function chekChekBox(){
    var chbRes = 0;
-   if (chb12.checked == true){
+   if ((chb12.checked == true)&&(chb11.checked == false)&&(chb13.checked == false)&&(chb14.checked == false)){
       chbRes += 0.5;
    }
-   if (chb13.checked == true){
+   if ((chb13.checked == true)&&(chb11.checked == false)&&(chb12.checked == false)&&(chb14.checked == false)){
       chbRes += 0.5;
    }
-   if (chb21.checked == true){
+   if ((chb12.checked == true)&&(chb13.checked == true)&&(chb11.checked == false)&&(chb14.checked == false)){
+      chbRes += 1;
+   }
+   if ((chb21.checked == true)&&(chb22.checked == false)&&(chb23.checked == false)&&(chb24.checked == false)){
       chbRes += 0.5;
    }
-   if (chb22.checked == true){
+   if ((chb22.checked == true)&&(chb21.checked == false)&&(chb23.checked == false)&&(chb24.checked == false)){
       chbRes += 0.5;
+   }
+   if ((chb21.checked == true)&&(chb22.checked == true)&&(chb23.checked == false)&&(chb24.checked == false)){
+      chbRes += 1;
    }
    return chbRes;
 }
